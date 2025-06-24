0612cc5ea45b7d9434a9384e53705c75b43122c3
nikosathanasopoulos <nikos.athanasopoulos@allwyn-lotterysolutions.com>
2025-06-24 10:07:33 +0300

    ...

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -66,7 +66,7 @@ class RecipeView extends View {
           <div class="recipe__info"> </div>
           <div class="recipe__info">
             <button class="btn--rectangle${this._data.bookmarked ? '-fill' : '' } btn--bookmark">
-                <span>🔖🔖🔖</span>
+                <span>🔖 Bookmark${this._data.bookmarked ? 'ed' : '' }</span>
             </button>
           </div>
         </div>
